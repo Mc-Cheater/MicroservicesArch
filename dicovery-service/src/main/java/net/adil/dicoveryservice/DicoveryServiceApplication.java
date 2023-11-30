16531ff3ea71dfa2ebdb297959f4aaec560e4ac2
mc-cheater <mcadilqarboua@gmail.com>
2023-11-30 16:09:40 +0100

    first commit

---
+++ dicovery-service/src/main/java/net/adil/dicoveryservice/DicoveryServiceApplication.java
@@ -0,0 +1,13 @@
+package net.adil.dicoveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DicoveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DicoveryServiceApplication.class, args);
+	}
+
+}
